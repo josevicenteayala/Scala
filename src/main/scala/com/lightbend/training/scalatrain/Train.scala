18759e1b444fed89e38a5d952190b7e39f6e538a
José Vicente Ayala Luna <vin001@gmail.com>
2020-06-10 04:39:00 -0500

    Add Time to stations seq

--- src/main/scala/com/lightbend/training/scalatrain/Train.scala
+++ src/main/scala/com/lightbend/training/scalatrain/Train.scala
@@ -1,8 +1,10 @@
 package com.lightbend.training.scalatrain
 
-class Train(val kind:String, val number:Int, schedule:Seq[Station]){
+class Train(val kind:String, val number:Int, schedule:Seq[(Time, Station)]){
   println("Size compare:: "+schedule.sizeCompare(2))
   require(schedule.sizeCompare(2) >= 0,"Schedule must contains at least 2 elements")
+
+  val stations: Seq[Station] = schedule.map(tuple => tuple._2)
 
   val map = Map((1,'a'),(2,'b'))
   map.contains(3)
--- src/test/scala/com/lightbend/training/scalatrinin/TrainTest.scala
+++ src/test/scala/com/lightbend/training/scalatrinin/TrainTest.scala
@@ -1,34 +1,34 @@
 package com.lightbend.training.scalatrinin
 
-import com.lightbend.training.scalatrain.{Station, Train}
+import com.lightbend.training.scalatrain.{Station, Time, Train}
 import org.scalatest.{Matchers, WordSpec}
 
 class TrainTest extends WordSpec with Matchers{
 
   "Create a train with 2 Stations" should {
     "create the train successfully" in {
-      val stations: Seq[Station] = Seq(Station("Madrid"),Station("London"))
+      val stations: Seq[(Time,Station)] = Seq((Time(),Station("Madrid")),(Time(),Station("London")))
       val train = new Train("faster", 1, stations)
     }
   }
 
   "Create a train with 3 Stations" should {
     "create the train successfully" in {
-      val stations: Seq[Station] = Seq(Station("Madrid"),Station("London"), Station("Paris"))
+      val stations: Seq[(Time,Station)] = Seq((Time(),Station("Madrid")),(Time(),Station("London")), (Time(),Station("Paris")))
       val train = new Train("faster", 1, stations)
     }
   }
 
   "Create a train with 1 station" should {
     "Throw an IllegalArgumentException because there is only 1 station and must be at least 2 stations" in {
-      val stations: Seq[Station] = Seq(Station("Madrid"))
+      val stations: Seq[(Time,Station)] = Seq((Time(),Station("Madrid")))
       an[IllegalArgumentException] should be thrownBy(new Train("faster", 1, stations))
     }
   }
 
   "Create a train with 0 station" should {
     "Throw an IllegalArgumentException because there is only 0 station and must be at least 2 stations" in {
-      val stations: Seq[Station] = Seq()
+      val stations: Seq[(Time,Station)] = Seq()
       an[IllegalArgumentException] should be thrownBy(new Train("faster", 1, stations))
     }
   }
